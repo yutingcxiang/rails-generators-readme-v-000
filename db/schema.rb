fde05a71e6f0e0e2117ee58959c2d1d55720b85d
Christine Xiang <yutingcxiang@gmail.com>
2018-09-06 04:36:57 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906043418) do
+ActiveRecord::Schema.define(version: 20180906043637) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20180906043418) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
+    t.string   "post_status"
   end
 
 end
